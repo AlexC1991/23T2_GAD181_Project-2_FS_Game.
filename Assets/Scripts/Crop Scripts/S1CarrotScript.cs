6e5bf1b3373e8dbbcca5de5eb1ddc2bda8d581bc
SAEStudentNathanHoward <114543371+SAEStudentNathanHoward@users.noreply.github.com>
2023-07-15 22:52:04 +1000

    bug fix

--- Assets/Scripts/Crop Scripts/S1CarrotScript.cs
+++ Assets/Scripts/Crop Scripts/S1CarrotScript.cs
@@ -1,4 +1,6 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace AlexzanderCowell
 {
@@ -9,11 +11,18 @@ namespace AlexzanderCowell
         private float startPlantTime; // Sets time of which the instance is initialized.
         private float nextPlantStageTime;
 
+        [Header("Progress Bar Settings")]
+        [SerializeField] private Slider progressBar;
+        private float barValue;
+
         private void Start()
         {
             startPlantTime = WorldClock.hourTime; // Sets float to whatever the current hour time is set at during the start phase of this prefab is spawned.
             nextPlantStageTime = startPlantTime + 2;
             gameObject.SetActive(true);
+
+            progressBar.maxValue = nextPlantStageTime - startPlantTime;
+            StartCoroutine("ProgressBar");
         }
         private void Update()
         {
@@ -30,6 +39,27 @@ namespace AlexzanderCowell
                 //gameObject.SetActive(false);
                 Destroy(gameObject);
             }
+        }
+
+        private IEnumerator ProgressBar()
+        {
+            while (true)
+            {
+                while (barValue <= progressBar.maxValue)
+                {
+                    barValue = nextPlantStageTime - currentTimeOfPlanting * 0.01f;
+                    UpdateProgressBar();
+
+                    yield return null;
+                }
+                
+                yield return null;
+            }
+        }
+
+        private void UpdateProgressBar()
+        {
+            progressBar.value = barValue;
         }
     }
 }
